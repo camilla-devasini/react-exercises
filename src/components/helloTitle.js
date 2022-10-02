3f15980c3dad516500dea6a61702e142a4b8a3a5
Camilla Devasini <camilladevasini@gmail.com>
2022-10-02 23:00:13 +0200

    add ex. components-06

--- src/components/helloTitle.js
+++ src/components/helloTitle.js
@@ -2,9 +2,14 @@ import React from "react";
 
 class SayHello extends React.Component {
     render() {
-        return <h1>Hello, World</h1>
+        return (
+            <>
+                <h1>Hello, World</h1>
+                <p>What a beautiful day!</p>
+            </>
+        )
     }
 }
 
 export default SayHello;
-// Write a Hello class components that renders the "Hello, World!" message within an h1 tag.
+
