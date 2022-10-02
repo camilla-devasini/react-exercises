4e3ca6477892ee39f2027e83ea134a8be7f54a8e
Camilla Devasini <camilladevasini@gmail.com>
2022-10-02 22:51:37 +0200

    add ex. components-04

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import * as ReactDOM from 'react-dom/client';
 import App from "./App.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render (
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
 )
+
+// https://github.com/facebook/react/blob/main/CHANGELOG.md#deprecations
+// il metodo ReactDOM.render è deprecato a partire dal 26 aprile 2022:
+// createRoot: New method to create a root to render or unmount.
+// Use it instead of ReactDOM.render. New features in React 18 don't work without it.
+
 
