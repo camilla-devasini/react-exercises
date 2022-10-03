2ad59a533d803543338bf5cc478d468c1f68aa29
Camilla Devasini <camilladevasini@gmail.com>
2022-10-03 14:32:00 +0200

    add ex. props-04

--- src/App.js
+++ src/App.js
@@ -8,10 +8,17 @@ class App extends React.Component {
         return (
             <div>
                 <SayHello />
-                <Welcome age="28" />
+                <Welcome name={<strong>Camilla</strong>} age="28" />
             </div>    
         )
     }
 }
 
 export default App;
+
+
+
+// What happens if the name prop is a JSX expression instead of a string? How do you pass a prop
+// that contains a JSX expression?
+// It is fine to pass a JSK expression (that will be converted into Javascript).
+// We need to include JSX code into curly brackets {}
