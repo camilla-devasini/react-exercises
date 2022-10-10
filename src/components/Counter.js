6765c7b1d4d6990225bfaa1fa3901e4a29ffd4b9
Camilla Devasini <camilladevasini@gmail.com>
2022-10-10 15:06:46 +0200

    add ex. state-04

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,5 @@
 import React from "react";
+import CounterDisplay from "./CounterDisplay";
 
 // Create a Counter class component with an internal state containing a count property,
 // initialized to 0. The Counter component should render the count property within an h1 tag,
@@ -14,37 +15,27 @@ class Counter extends React.Component {
             count: this.props.initialValue
         }
 
+       
         setInterval(() => {
-            this.setState((state) => {
-                return {count: state.count + this.props.increment}
+          this.setState((state) => {
+              return {count: state.count + this.props.increment}
 
-            })
+          })
         }, this.props.interval )
-    }
+      }
 
 
-
+    
     render() {
-        return <h1>{this.state.count}</h1>
+        return <CounterDisplay counterValue={this.state.count} />
+        
     }
 }
 
-<<<<<<< HEAD
-// import { useState, useEffect } from "react"
 
 
-// // Create a Counter class component with an internal state containing a count property,
-// // initialized to 0. The Counter component should render the count property within an h1 tag,
-// // and the count property should be incremented by 1 every second
 
-// export function Counter() {
-//     const [counter, setCounter] = useState(1000); //1. definire la variabile counter - 
-//     //2. definire il valore iniziale di counter -
-//     // 3. definire qual è la chiamata che modificherà il valore di counter
 
-//     // quando la pagina è totalmente caricata ==> parte il contatore.
-//     useEffect(() => {
-=======
 // Svolto durante la call 1:1 
 // import { useState, useEffect } from "react"
 
@@ -60,7 +51,6 @@ class Counter extends React.Component {
 //     // quando la pagina è totalmente caricata ==> parte il contatore.
 //     
 //         useEffect(() => {
->>>>>>> state-03
 //         const interval = setInterval(() => {
 //           setCounter(counter => counter - 1); //la chiamata che modifica il valore del counter
 //         }, 1000);
@@ -82,9 +72,5 @@ class Counter extends React.Component {
 
 // }
 
-<<<<<<< HEAD
-export default Counter;
-=======
 export default Counter;
 
->>>>>>> state-03
---
+++ src/components/CounterDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class CounterDisplay extends React.Component {
+    render() {
+        return <h1>{this.props.counterValue}</h1> 
+    } 
+    
+}
+export default CounterDisplay;
+  
+  
+
