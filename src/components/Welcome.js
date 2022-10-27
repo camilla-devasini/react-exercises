39fc00e206c71dca01642641a4f49dfc8999a65a
Camilla Devasini <camilladevasini@gmail.com>
2022-10-27 17:12:44 +0200

    add ex. functional-component-01

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,24 +1,24 @@
-import React from "react";
+
 import Age from "./Age";
 
 
 // this.props.age > 18 && this.props.age < 65 && 
-class Welcome extends React.Component {
-    render() {
-        let adultRange = this.props.age > 18 || this.props.age < 65; 
-        // 19 - 64                                      // 0-18 oppure 65-100
-    
-        if (adultRange && this.props.name == 'Jhon' || (!adultRange || this.props.name != 'Jhon')) {
-            
-            return (
-                <>
-                    <p className="welcome">Welcome {this.props.name && <strong>{this.props.name}</strong>}!</p>
-                    <Age age={this.props.age} />
-                </>
-            )
-        }      
-    }
-} 
+function Welcome(props) {
+  
+    let adultRange = props.age > 18 || props.age < 65; 
+    // 19 - 64                                      // 0-18 oppure 65-100
+
+    if (adultRange && props.name == 'Jhon' || (!adultRange || props.name != 'Jhon')) {
+        
+        return (
+            <>
+                <p className="welcome">Welcome {props.name && <strong>{props.name}</strong>}!</p>
+                <Age age={props.age} />
+            </>
+        )
+    }      
+}
+
 
 //What happens if no name prop is passed to the Welcome component?
 // In that case no name will be rendered, only the "Welcome!" string. 
